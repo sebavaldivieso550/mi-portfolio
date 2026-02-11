1ccc10b564485b2da99abb801ba3601d7ed881ee
sebavaldivieso550 <sebavaldivieso550@gmail.com>
2026-02-11 11:06:25 -0300

    Primer paso: estructura inicial

---
+++ src/components/Hero.js
@@ -0,0 +1,3 @@
+<div>
+    Hola, soy Sebastián Valdivieso, estudiante de Ingeniería. Construyo software con React y optimizo hardware desde la raíz. Mi enfoque es la resolución de problemas, ya sea en una línea de código o en una placa madre.
+</div>
